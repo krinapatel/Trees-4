10e45e6f0939c9683f1214ec3ca32e0ec74c6015
Krina Patel <kp42942n@pace.edu>
2022-04-21 16:43:21 -0400

    Create lowestCommonAncestorBT.js

---
+++ lowestCommonAncestorBT.js
@@ -0,0 +1,58 @@
+// Time Complexity : O(n)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+
+ let pathP;
+ let pathQ;
+ var recursion = function(root, p, q, path) {
+     //base case
+     if(root === null) return null;
+     console.log(root.val);
+     //logic
+     if(p.val === root.val) {
+         pathP = [...path];
+         pathP.push(p);
+         pathP.push(p);
+     }
+     if(q.val === root.val) {
+         pathQ = [...path];
+         pathQ.push(q);
+         pathQ.push(q);
+     }
+         
+     //action
+     path.push(root);
+     
+     //recursion
+     if(pathP.length === 0 || pathQ.length === 0) recursion(root.left, p, q, path);
+     if(pathP.length === 0 || pathQ.length === 0) recursion(root.right, p, q, path);
+     
+     //backtrack
+     path.pop();
+ }
+ 
+ var lowestCommonAncestor = function(root, p, q) {
+     pathP = [];
+     pathQ = [];
+     recursion(root, p, q, []);
+     //console.log(pathP);
+     for(let i = 0; i < pathP.length; i++) {
+         if(pathP[i] !== pathQ[i]) return pathP[i-1];
+     }
+     return null;
+ };
