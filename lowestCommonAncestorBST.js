3aaca69a3e1de5b46a244db7e6dcd2a4b8efd521
Krina Patel <kp42942n@pace.edu>
2022-04-21 16:44:12 -0400

    renaming file

--- lowestCommonAncestorBST.js
+++ lowestCommonAncestorBST.js
@@ -19,9 +19,9 @@
 // Any problem you faced while coding this : No
 
 //Recursion Soltion
-var lowestCommonAncestor = function(root, p, q) {
-    if(p.val < root.val && q.val < root.val) return lowestCommonAncestor(root.left, p, q);
-    else if(p.val > root.val && q.val > root.val) return lowestCommonAncestor(root.right, p, q);
+var lowestCommonAncestorBST = function(root, p, q) {
+    if(p.val < root.val && q.val < root.val) return lowestCommonAncestorBST(root.left, p, q);
+    else if(p.val > root.val && q.val > root.val) return lowestCommonAncestorBST(root.right, p, q);
     return root;
 };
 
@@ -29,7 +29,7 @@ var lowestCommonAncestor = function(root, p, q) {
 // Space Complexity : O(1)
 
 //Non recursion Solution
-var lowestCommonAncestor = function(root, p, q) {
+var lowestCommonAncestorBST = function(root, p, q) {
     while(true) {
         if(p.val < root.val && q.val < root.val) root = root.left;
         else if(p.val > root.val && q.val > root.val) root = root.right;
